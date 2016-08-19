d7c4935d0d88af4918eff7ff059704454c90016c
Charles Ewel <Charles.Ewel@gmail.com>
2016-08-19 09:12:44 -0700

    add new questions

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('question');
+  },
+  actions: {
+    saveQuestion(params) {
+      var newQuestion = this.store.createRecord('question', params);
+      newQuestion.save();
+      this.transitionTo('index');
+    }
   }
 });
